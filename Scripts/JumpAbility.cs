9d28e072f665c621b8a5cc143970a1d481fb1cb2
Illia Rybas <39777055+kor1b@users.noreply.github.com>
2020-10-19 19:33:54 +0300

    Add files via upload

---
+++ Scripts/JumpAbility.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Bimicore.BSG.StateMachinePattern;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Bimicore.BSG.ThirdPerson
+{
+	public class JumpAbility : MovementInAirAbstractAbility, IState
+	{
+		public int StateRestrictorsCount { get; set; } = 0;
+
+		private const float LOW_JUMP_FALL_ACCELERATION_MULTP = 2;
+		
+		[Header ("Jump")] 
+		
+		[SerializeField] private float jumpHeight = 3f;
+		[ValidateInput ("IsNotGreaterThanJumpHeight", "Carefully, minJumpHeight should be less than jumpHeight.")]
+		[SerializeField] private float minJumpHeight = 0.5f;
+
+		[Space]
+		
+		[Tooltip ("Acceleration that pushes player upward")]
+		[SerializeField] private float jumpUpAcceleration = 2.6f;
+		[SerializeField] private bool enableCustomLowJumpFallAcceleration = false; // Used only to expand Inspector
+		[EnableIf ("enableCustomLowJumpFallAcceleration")]
+		[SerializeField] private float lowJumpFallAcceleration;
+
+		private float _jumpStartHeight;
+		private bool _jumpButtonReleasedInAir = false;
+
+		#region Inspector
+
+		private bool IsNotGreaterThanJumpHeight (float value) => value <= jumpHeight;
+
+		#endregion
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			TrySetDefaultLowJumpFallAcceleration();
+		}
+
+		private void TrySetDefaultLowJumpFallAcceleration()
+		{
+			if (!enableCustomLowJumpFallAcceleration)
+				lowJumpFallAcceleration = physicalProperties.fallAcceleration * LOW_JUMP_FALL_ACCELERATION_MULTP;
+		}
+
+		public async void OnEnter()
+		{
+			CommonEnter();
+
+			view.StartAnimation (animatorManager);
+
+			thirdPersonRotation.TryAlignHorizontalLookRotationWith (inputSystem.InputHorizontalMovementDirection);
+			
+			CalculateTargetVelocityDirection (inputSystem.InputHorizontalMovementDirection);
+
+			Jump();
+
+			if (inputSystem.InputHorizontalMovementDirection != 0)
+			{
+				await IncreaseTargetVelocity (physicalProperties.accelerationInAir, maxHorizontalSpeed);
+			}
+		}
+
+		public void Tick()
+		{
+			CommonTick();
+		}
+
+		public void FixedTick()
+		{
+			AddGravityToJumpUp();
+
+			CommonFixedTick();
+		}
+
+		public void OnExit()
+		{
+			CommonExit();
+
+			//Reset for a new jump
+			_jumpButtonReleasedInAir = false;
+
+			//Restricts Bunny Hop possibility
+			inputSystem.JumpButtonHold = false;
+		}
+
+		/// <summary>
+		/// Add vertical velocity to character 
+		/// </summary>
+		private void Jump()
+		{
+			_jumpStartHeight = physicalProperties.Rb.position.y;
+			physicalProperties.targetVelocity.y = 0f;
+
+			float jumpUpSpeed = CalculateJumpSpeed (jumpHeight, jumpUpAcceleration);
+
+			physicalProperties.targetVelocity += Vector3.up * jumpUpSpeed;
+		}
+
+		private float CalculateJumpSpeed (float height, float upwardAcceleration)
+		{
+			//formula sqrt(2 * jumpHeight * upward_acceleration)
+			return Mathf.Sqrt (2 * height * upwardAcceleration);
+		}
+		
+		private void AddGravityToJumpUp()
+		{
+			float downwardAcceleration = GetDownwardAcceleration();
+			AddGravity (downwardAcceleration);
+		}
+		
+		private float GetDownwardAcceleration()
+		{
+			if (_jumpButtonReleasedInAir && IsMinJumpHeightReached())
+			{
+				return lowJumpFallAcceleration;
+			}
+
+			return physicalProperties.fallAcceleration;
+		}
+
+		public bool IsMinJumpHeightReached() => physicalProperties.Rb.position.y >= _jumpStartHeight + minJumpHeight;
+		
+		private void JumpButtonRelease() => _jumpButtonReleasedInAir = true;
+
+		protected override void SubscribeOnInput()
+		{
+			base.SubscribeOnInput();
+
+			inputSystem.jumpInputCancelEventHandler += JumpButtonRelease;
+		}
+
+		protected override void UnsubscribeFromInput()
+		{
+			base.UnsubscribeFromInput();
+
+			inputSystem.jumpInputCancelEventHandler -= JumpButtonRelease;
+		}
+	}
+}
---
+++ Scripts/ThirdPersonEnvironmentScanner.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using Bimicore.BSG.CoolTools;
+using UnityEngine;
+
+namespace Bimicore.BSG.ThirdPerson
+{
+	public class ThirdPersonEnvironmentScanner : MonoBehaviour
+	{
+// Disabled warnings like 'Field is never assigned to, and will always have its default value'
+// because structs initialization is in Inspector
+#pragma warning disable CS0649
+
+		[SerializeField] private GroundCheck groundCheck;
+
+		[Space] 
+		
+		[SerializeField] private ObstacleCheck obstacleCheck;
+
+		private ThirdPersonMovementPhysicalProperties _physicalProperties;
+
+#pragma warning restore
+
+		private void Awake()
+		{
+			_physicalProperties = GetComponent<ThirdPersonMovementPhysicalProperties>();
+		}
+
+		private void Update()
+		{
+			DebugPlus.LogOnScreen("OnGround: " + IsGrounded());
+			DebugPlus.LogOnScreen("GroundType: " + GetGroundTypeBelow());
+			DebugPlus.LogOnScreen("IsOnSlide: " + IsOnSlide());
+		}
+
+		// Warning: method can return true if player are very close to the ground. Modify distToGround to control that.
+		public bool IsGrounded()
+		{
+			var supposedGround = GetGroundHit(groundCheck.ray);
+			
+			if (supposedGround.collider)
+			{
+				if (LayersCoolTool.IsSameLayer (groundCheck.groundMask, supposedGround.collider.gameObject.layer))
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		private RaycastHit GetGroundHit(RaySettings raySettings)
+		{
+			Physics.Raycast(raySettings.origin.position,
+			                -transform.up,
+			                out var hit,
+			                raySettings.checkDistance,
+			                groundCheck.groundMask);
+
+			return hit;
+		}
+		
+		public GroundType GetGroundTypeBelow()
+		{
+			GroundType groundType = GetGroundTypeByRay(GetRayDown());
+
+			return groundType;
+		}
+
+		private RaySettings GetRayDown()
+		{
+			GroundType groundTypeInFront = GetGroundTypeByRay(groundCheck.slope.frontRay);
+
+			// If the ground in front is lower (or absent) than the current ground, then hold on to the last on the current ground.
+			if (IsGroundGoesDownOrAbsent(groundTypeInFront))
+			{
+				return groundCheck.slope.backRay;
+			}
+
+			return groundCheck.slope.frontRay;
+		}
+
+		private GroundType GetGroundTypeByRay(RaySettings raySettings)
+		{
+			var ground = GetGroundHit(raySettings);
+
+			float groundAngle = GetGroundAngle(ground);
+
+			return GetGroundTypeByAngle(groundAngle);
+		}
+		
+		public float GetGroundAngle()
+		{
+			RaycastHit hit = GetGroundHit(GetRayDown());
+
+			// Get the SIGNED angle to slope and multiply it with -1 to define difference btw ascend and descend
+			float slopeAngle = Vector2.SignedAngle(hit.normal, Vector2.up) * -1;
+
+			return slopeAngle;
+		}
+
+		private float GetGroundAngle(RaycastHit hit) => 
+			Vector2.Angle(hit.normal, GetMovementDirectionBasedOnVelocity());
+
+		
+		private Vector3 GetMovementDirectionBasedOnVelocity()
+		{
+			Vector3 directionRelativeToMovement;
+
+			if (_physicalProperties.targetVelocityDirection.x != 0)
+				directionRelativeToMovement = new Vector3(Mathf.Sign(_physicalProperties.targetVelocity.x), 0, 0);
+			else // if character doesn't move, use face direction
+				directionRelativeToMovement = new Vector3(transform.forward.x, 0, 0);
+
+			return directionRelativeToMovement;
+		}
+
+		private GroundType GetGroundTypeByAngle(float groundAngle)
+		{
+			if (Math.Abs(groundAngle - 90) <= groundCheck.slope.groundAngleTolerance)
+				return GroundType.Flat;
+
+			if (groundAngle < 90 - groundCheck.slope.groundAngleTolerance && groundAngle > 0)
+				return GroundType.Descent;
+
+			if (groundAngle > 90 + groundCheck.slope.groundAngleTolerance)
+				return GroundType.Ascent;
+
+			return GroundType.NoGround;
+		}
+
+		private bool IsGroundGoesDownOrAbsent(GroundType groundTypeInFront) =>
+			groundTypeInFront == GroundType.NoGround || groundTypeInFront == GroundType.Descent;
+
+		public bool IsOnSlide()
+		{
+			if (GetGroundTypeBelow() == GroundType.Ascent || GetGroundTypeBelow() == GroundType.Descent)
+			{
+				return Mathf.Abs(GetGroundAngle()) > groundCheck.slide.minAngleToSlide &&
+				       Mathf.Abs(GetGroundAngle()) < groundCheck.slide.maxAngleToSlide;
+			}
+
+			return false;
+		}
+
+		public int GetSlideDirection() => 
+			(int) Mathf.Sign(GetGroundHit(GetRayDown()).normal.x);
+
+		public bool IsObstacleInDirection(float direction) =>
+			Physics.Raycast(obstacleCheck.ray.origin.position,
+			                Vector2.right * direction,
+			                obstacleCheck.ray.checkDistance,
+			                obstacleCheck.obstacleMask);
+
+		private void OnDrawGizmos()
+		{
+			// Ground check ray
+			Gizmos.color = Color.red;
+
+			Gizmos.DrawRay(groundCheck.ray.origin.position + Vector3.up * 0.2f,
+			               -Vector3.up * (groundCheck.ray.checkDistance + 0.1f));
+
+			// Front slope check ray
+			Gizmos.color = Color.black;
+
+			Gizmos.DrawRay(groundCheck.slope.frontRay.origin.position,
+			               -transform.up * groundCheck.slope.frontRay.checkDistance);
+
+			// Back slope check ray
+			Gizmos.color = Color.black;
+
+			Gizmos.DrawRay(groundCheck.slope.backRay.origin.position,
+			               -transform.up * groundCheck.slope.backRay.checkDistance);
+		}
+
+#pragma warning disable CS0649
+
+		[Serializable]
+		private struct GroundCheck
+		{
+			public LayerMask groundMask;
+			public RaySettings ray;
+			[Space] public SlopeCheck slope;
+			[Space] public SlideCheck slide;
+		}
+
+		[Serializable]
+		private struct SlopeCheck
+		{
+			public float groundAngleTolerance;
+			public RaySettings frontRay;
+			public RaySettings backRay;
+		}
+
+		[Serializable]
+		private struct SlideCheck
+		{
+			public float minAngleToSlide;
+			public float maxAngleToSlide;
+		}
+
+		[Serializable]
+		private struct ObstacleCheck
+		{
+			public LayerMask obstacleMask;
+			public RaySettings ray;
+		}
+
+		[Serializable]
+		private struct RaySettings
+		{
+			public Transform origin;
+			public float checkDistance;
+		}
+
+#pragma warning restore
+	}
+}
